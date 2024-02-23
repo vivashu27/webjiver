6c26c763f86bb03576d571b7ba6b7af05be17939
luffy27 <52876924+vivashu27@users.noreply.github.com>
2024-02-23 14:13:31 +0530

    Create webjiver.sh

---
+++ webjiver.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+katana 2> /dev/null
+if [ "$?" != "" ];then
+    echo -e "\e[31mPlease Download katana first\e[0m"
+    exit
+fi
+
+echo "Enter the domain to scan"
+read domain
+
+echo "Enter the output file"
+read output
+
+echo -e "\e[32mFinding subdomains....\e[0m"
+subfinder -silent -d $domain > dom.tmp
+amass  enum -nocolor -d $domain -o amass -silent
+cat amass | awk -F "-->" '{print $3}' | grep -E -v "RIROrganization|Netblock" | cut -d "(" -f 1 | grep -v "\\s+" | sed -E 's/^\s+//g' | grep -E -v "\S+:\S+:\S+:\S+::\S+" >>dom.tmp
+echo -e "\e[32mFinding open ports....\e[0m"
+naabu -silent -list dom1.tmp -o ports.tmp
+cat ports.tmp | sed -e "s/^/https:\/\//g" > https.tmp
+cat ports.tmp | sed  -e "s/^/http:\/\//g" > http.tmp
+
+echo -e "\e[32mChecking the connectivity....\e[0m"
+while read http; do
+    if [ "$(httpx --timeout 3 --no-verify $http | grep -o 'ReadTimeout')" == "" ]; then
+        echo $http >> validhttp.tmp
+    fi
+done < http.tmp
+
+while read https; do
+    if [ "$(httpx --timeout 3 --no-verify $https | grep -o 'ReadTimeout')" == "" ]; then
+        echo $https >> validhttp.tmp
+    fi
+done < https.tmp
+
+
+echo -e "\e[32mSpidering and Finding endpoints....\e[0m"
+paramspider -l dom1.tmp > param.tmp
+katana -list validhttp.tmp -o katana.tmp
+
+while read domain; do
+	cat results/$domain.txt 2> /dev/null  1>> $output.tmp
+done < dom1.tmp
+
+cat katana.tmp > $output.tmp
+cat $output.tmp | sort | uniq > $output
+
+echo "Saved to your output file: $output"
