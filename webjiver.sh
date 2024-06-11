d92398ee86a23ada2aba6226a9cbe0b6209d2861
luffy27 <52876924+vivashu27@users.noreply.github.com>
2024-06-11 14:31:41 +0530

    Some logical error
    Fixed some logical error in the code...;-)

--- webjiver.sh
+++ webjiver.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-katana 2> /dev/null
-if [ "$?" != "" ];then
-    echo -e "\e[31mPlease Download katana first\e[0m"
-    exit
-fi
+#katana 2> /dev/null
+#if [ "$?" != "" ];then
+ #   echo -e "\e[31mPlease Download katana first\e[0m"
+  #  exit
+#fi
 
 echo "Enter the domain to scan"
 read domain
@@ -17,7 +17,7 @@ subfinder -silent -d $domain > dom.tmp
 amass  enum -nocolor -d $domain -o amass -silent
 cat amass | awk -F "-->" '{print $3}' | grep -E -v "RIROrganization|Netblock" | cut -d "(" -f 1 | grep -v "\\s+" | sed -E 's/^\s+//g' | grep -E -v "\S+:\S+:\S+:\S+::\S+" | grep -E -v "\S+:\S+:\S+::\S+:\S+"|grep -E -v "\S+:\S+:\S+:\S+:\S+:\S+:\S+:\S+" | grep -E -v "\S+:\S+:\S+::\S+" >dom.tmp
 echo -e "\e[32mFinding open ports....\e[0m"
-naabu -silent -list dom.tmp -o ports.tmp
+naabu -silent -top-ports 1000 -list dom.tmp -o ports.tmp
 cat ports.tmp | sed -e "s/^/https:\/\//g" > https.tmp
 cat ports.tmp | sed  -e "s/^/http:\/\//g" > http.tmp
 
@@ -37,13 +37,13 @@ done < https.tmp
 
 echo -e "\e[32mSpidering and Finding endpoints....\e[0m"
 paramspider -l dom.tmp > param.tmp
-katana -list validhttp.tmp -o katana.tmp
+cat validhttp.tmp | hakrawler -insecure -u  > hakcraw.tmp
 
 while read domain; do
 	cat results/$domain.txt 2> /dev/null  1>> $output.tmp
 done < dom.tmp
 
-cat katana.tmp > $output.tmp
+cat hakcraw.tmp >> $output.tmp
 cat $output.tmp | sort | uniq > $output
 
 echo "Saved to your output file: $output"
