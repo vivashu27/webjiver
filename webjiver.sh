6e1d4dbab374a0f190c74277507e5faf60cfdce5
luffy27 <52876924+vivashu27@users.noreply.github.com>
2025-03-11 17:25:22 +0530

    Update webjiver.sh

--- webjiver.sh
+++ webjiver.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-katana 2> /dev/null
-if [ "$?" != "" ];then
-    echo -e "\e[31mPlease Download katana first\e[0m"
-    exit
-fi
-
 echo "Enter the domain to scan"
 read domain
 
@@ -17,15 +11,15 @@ subfinder -silent -d $domain > d.tmp
 assetfinder $domain >> d.tmp
 cat d.tmp | sort | uniq > dom.tmp
 rm d.tmp
-amass  enum -nocolor -d $domain -o amass -silent
-cat amass | awk -F "-->" '{print $3}' | grep -E -v "RIROrganization|Netblock" | cut -d "(" -f 1 | grep -v "\\s+" | sed -E 's/^\s+//g' | grep -E -v "\S+:\S+:\S+:\S+::\S+" | grep -E -v "\S+:\S+:\S+::\S+:\S+"|grep -E -v "\S+:\S+:\S+:\S+:\S+:\S+:\S+:\S+" | grep -E -v "\S+:\S+:\S+::\S+" | grep -E -v "\S+:\S+:\S+:\S+::">>dom.tmp
+#amass  enum -nocolor -d $domain -o amass -silent
+#cat amass | awk -F "-->" '{print $3}' | grep -E -v "RIROrganization|Netblock" | cut -d "(" -f 1 | grep -v "\\s+" | sed -E 's/^\s+//g' | grep -E -v "\S+:\S+:\S+:\S+::\S+" | grep -E -v "\S+:\S+:\S+::\S+:\S+"|grep -E -v "\S+:\S+:\S+:\S+:\S+:\S+:\S+:\S+" | grep -E -v "\S+:\S+:\S+::\S+" | grep -E -v "\S+:\S+:\S+:\S+::">>dom.tmp
 echo -e "\e[32mFinding open ports....\e[0m"
 naabu -silent -top-ports 1000 -list dom.tmp -o ports.tmp
 cat ports.tmp | sed -e "s/^/https:\/\//g" > https.tmp
 cat ports.tmp | sed  -e "s/^/http:\/\//g" >> https.tmp
 
 echo -e "\e[32mChecking the connectivity....\e[0m"
-cat https.tmp | httpx -fr -mc 200,302,404 -nc -o validhttp.tmp
+cat https.tmp | httpx -mc 200,302,403,404 -nc -o validhttp.tmp
 
 echo -e "\e[32mSpidering and Finding endpoints....\e[0m"
 paramspider -l dom.tmp > param.tmp
@@ -36,6 +30,6 @@ while read domain; do
 done < dom.tmp
 
 cat hakcraw.tmp >> $output.tmp
-cat $output.tmp | sort | uniq > $output
+cat $output.tmp | sort | uniq | uro > $output
 
 echo "Saved to your output file: $output"
